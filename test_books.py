6a6aca35011474cb6d887be27b1959b49fae582c
Andre29an <2023.manevskyi.andrii@ktbp.net.ua>
2026-05-11 12:57:57 +0300

    Add test for default book status

--- test_books.py
+++ test_books.py
@@ -4,4 +4,11 @@ class TestBooks:
         assert response.status_code == 200
         assert response.get_json() == []
 
-    def test_create_book(self, client):
+
+    def test_create_book_default_status(self, client):
+        """Книга створюється зі статусом за замовчуванням"""
+        response = client.post("/api/books", json={
+            "title": "Test Book",
+            "created_by": "Ім'я Прізвище",
+        })
+        assert response.status_code == 201
